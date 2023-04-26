5ec7f984844f5c58105bb51f031da00e78ccec4a
Dante Ibanez <dantedavidibanez2@gmail.com>
2023-04-26 15:00:52 -0700

    Introduced skeleton code based on ANN2

--- Source.java
+++ Source.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class Source{
     public static void main(String [] args){
-
+        //main to read in file
     }
 
     public static void firstLayer(){
-
+        //first layer
     }
 
     public static void secondLayer(){
-
+        //second layer
     }
 
     public static void finalLayer(){
-        
+        //final layer
     }
 }
