ebfe48720b70985b4c9dbaf92da8a0488009f060
Dante Ibanez <dantedavidibanez2@gmail.com>
2023-04-26 14:59:16 -0700

    Introduced skeleton code from ANN2

---
+++ Source.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Source{
+    public static void main(String [] args){
+
+    }
+
+    public static void firstLayer(){
+
+    }
+
+    public static void secondLayer(){
+
+    }
+
+    public static void finalLayer(){
+        
+    }
+}
